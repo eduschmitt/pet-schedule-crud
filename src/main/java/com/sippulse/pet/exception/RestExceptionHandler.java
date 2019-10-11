c46244fa5c6c5504961e73fe0f0e36ab4a347cc7
Eduardo J Schmitt <eduschmitt@hotmail.com>
2019-10-11 20:22:26 -0300

    Adicionando handler para exceções de outros tipos.

--- src/main/java/com/sippulse/pet/exception/RestExceptionHandler.java
+++ src/main/java/com/sippulse/pet/exception/RestExceptionHandler.java
@@ -5,10 +5,15 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.sippulse.pet.exception.ServiceException.TipoExcecao;
@@ -44,6 +49,54 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         apiError.setMessage("Ocorreu uma exceção inesperada.");
         apiError.setDebugMessage(ex.getMessage());
         return buildResponseEntity(apiError);
+    }
+    
+    /**
+     * Handler para quando um campo de request requerido não foi informado.
+     */
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+        String error = ex.getParameterName() + " parameter is missing";
+        return buildResponseEntity(new ApiError(BAD_REQUEST, error, ex));
+    }
+    
+    /**
+     * Handler para quando falha uma validação de @Valid
+     */
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        ApiError apiError = new ApiError(BAD_REQUEST);
+        apiError.setMessage("Validation error");
+        return buildResponseEntity(apiError);
+    }
+    
+    /**
+     * Método para tratar informações inválidas nos campos da requisição.
+     * Ex: valor inválido em um campo enum.
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(javax.validation.ConstraintViolationException.class)
+    protected ResponseEntity<Object> handleConstraintViolation(
+            javax.validation.ConstraintViolationException ex) {
+        ApiError apiError = new ApiError(BAD_REQUEST);
+        apiError.setMessage("Validation error");
+        return buildResponseEntity(apiError);
+    }
+    
+    /**
+     * Handler para quando o JSON é mal formado.
+     */
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error = "Malformed JSON request";
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
     }
 
     private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
